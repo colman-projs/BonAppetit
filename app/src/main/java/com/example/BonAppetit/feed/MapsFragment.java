0fda7dec82b018b2308e000b5ce439eb663186f3
לירן <liran.hassan@gmail.com>
2022-03-18 18:37:43 +0200

    Liran - Commit map

--- app/src/main/java/com/example/BonAppetit/feed/MapsFragment.java
+++ app/src/main/java/com/example/BonAppetit/feed/MapsFragment.java
@@ -17,7 +17,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.BonAppetit.R;
 
+import java.util.ArrayList;
+
 public class MapsFragment extends Fragment {
+
+    // below are the latitude and longitude
+    // of 4 different locations.,,
+    LatLng carmel_market = new LatLng(32.06964105711388, 34.76896914543392); // carmel market
+    LatLng vitrina_tel_aviv = new LatLng(32.062233374685995, 34.77184962122501); // vitrina tel-aviv
+
+    // creating array list for adding all our locations.
+    private ArrayList<LatLng> locationArrayList;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -32,9 +42,32 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            // in below line we are initializing our array list.
+            locationArrayList = new ArrayList<>();
+
+            // on below line we are adding our
+            // locations in our array list.
+            locationArrayList.add(carmel_market);
+            locationArrayList.add(vitrina_tel_aviv);
+//            locationArrayList.add(NewCastle);
+//            locationArrayList.add(Brisbane);
+
+//            LatLng sydney = new LatLng(-34, 151);
+//            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            for (int i = 0; i < locationArrayList.size(); i++) {
+
+                // below line is use to add marker to each location of our array list.
+                googleMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Marker"));
+
+                // below lin is use to zoom our camera on map.
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+
+                // below line is use to move our camera to the specific location.
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(locationArrayList.get(i)));
+            }
         }
     };
 
