3db2fadeabc3854d0e70194db15f9df84aed50e5
Bar Pisahov <barpesahov@gmail.com>
2022-03-20 19:29:16 +0200

    Pass filters to Restaurant List

--- app/src/main/java/com/example/BonAppetit/feed/RestaurantListRvViewModel.java
+++ app/src/main/java/com/example/BonAppetit/feed/RestaurantListRvViewModel.java
@@ -1,43 +1,58 @@
 package com.example.BonAppetit.feed;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.BonAppetit.model.Model;
 import com.example.BonAppetit.model.Restaurant;
 import com.example.BonAppetit.model.RestaurantType;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class RestaurantListRvViewModel extends ViewModel {
-    private final MutableLiveData<Set<RestaurantType>> filters = new MutableLiveData<>();
+    private final ArrayList<RestaurantType> filters = new ArrayList<>();
 
     LiveData<List<Restaurant>> data;
-    LiveData<List<Restaurant>> filteredList;
 
     public RestaurantListRvViewModel() {
         data = Model.instance.getAll();
     }
 
     public LiveData<List<Restaurant>> getData() {
+        ArrayList<Restaurant> filteredRestaurants = new ArrayList<>();
+
+        if (data.getValue() != null) {
+            for (Restaurant restaurant : data.getValue()) {
+                for (RestaurantType type : filters) {
+                    if (type.isChecked() && restaurant.getRestaurantTypeId() == type.getId()) {
+                        filteredRestaurants.add(restaurant);
+                    }
+                }
+            }
+        }
+
+//        data = restaurants;
+
         return data;
     }
 
-    public LiveData<Set<RestaurantType>> getFilters() {
+    public ArrayList<RestaurantType> getFilters() {
         return filters;
     }
 
     public void updateFilter(RestaurantType filter) {
-        // If filter exists
-        
-        // Remove Filter
+        boolean isExists = false;
+        for (RestaurantType type : filters) {
+            if (type.getId() == filter.getId()) {
+                isExists = true;
+            }
+        }
 
-        // Else
-
-        // Add filter
+        if (isExists) {
+            // Add filter to filters
+        } else {
+            filters.add(filter);
+        }
     }
 }
